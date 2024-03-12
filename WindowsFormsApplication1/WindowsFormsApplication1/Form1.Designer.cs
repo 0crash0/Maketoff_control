b10b1640ded8985e968bfca2ba35a2693ce95c9e
Saprin Alex <0crash0@bk.ru>
2024-03-12 18:16:24 +0300

    initial

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connect_btn = new System.Windows.Forms.Button();
+            this.console_txt = new System.Windows.Forms.TextBox();
+            this.tst1 = new System.Windows.Forms.Button();
+            this.tst2 = new System.Windows.Forms.Button();
+            this.tst3 = new System.Windows.Forms.Button();
+            this.tst4 = new System.Windows.Forms.Button();
+            this.tst5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connect_btn
+            // 
+            this.connect_btn.Location = new System.Drawing.Point(7, 10);
+            this.connect_btn.Name = "connect_btn";
+            this.connect_btn.Size = new System.Drawing.Size(75, 23);
+            this.connect_btn.TabIndex = 5;
+            this.connect_btn.Text = "connect";
+            this.connect_btn.UseVisualStyleBackColor = true;
+            this.connect_btn.Click += new System.EventHandler(this.connect_btn_Click);
+            // 
+            // console_txt
+            // 
+            this.console_txt.ForeColor = System.Drawing.Color.YellowGreen;
+            this.console_txt.Location = new System.Drawing.Point(42, 39);
+            this.console_txt.Multiline = true;
+            this.console_txt.Name = "console_txt";
+            this.console_txt.Size = new System.Drawing.Size(247, 36);
+            this.console_txt.TabIndex = 6;
+            this.console_txt.Text = "Console:";
+            // 
+            // tst1
+            // 
+            this.tst1.Location = new System.Drawing.Point(7, 88);
+            this.tst1.Name = "tst1";
+            this.tst1.Size = new System.Drawing.Size(75, 23);
+            this.tst1.TabIndex = 7;
+            this.tst1.Text = "1tst";
+            this.tst1.UseVisualStyleBackColor = true;
+            this.tst1.Click += new System.EventHandler(this.tst1_Click);
+            // 
+            // tst2
+            // 
+            this.tst2.Location = new System.Drawing.Point(89, 87);
+            this.tst2.Name = "tst2";
+            this.tst2.Size = new System.Drawing.Size(75, 23);
+            this.tst2.TabIndex = 8;
+            this.tst2.Text = "2tst";
+            this.tst2.UseVisualStyleBackColor = true;
+            this.tst2.Click += new System.EventHandler(this.tst2_Click);
+            // 
+            // tst3
+            // 
+            this.tst3.Location = new System.Drawing.Point(171, 87);
+            this.tst3.Name = "tst3";
+            this.tst3.Size = new System.Drawing.Size(75, 23);
+            this.tst3.TabIndex = 9;
+            this.tst3.Text = "3tst";
+            this.tst3.UseVisualStyleBackColor = true;
+            this.tst3.Click += new System.EventHandler(this.tst3_Click);
+            // 
+            // tst4
+            // 
+            this.tst4.Location = new System.Drawing.Point(7, 117);
+            this.tst4.Name = "tst4";
+            this.tst4.Size = new System.Drawing.Size(75, 23);
+            this.tst4.TabIndex = 10;
+            this.tst4.Text = "4tst";
+            this.tst4.UseVisualStyleBackColor = true;
+            this.tst4.Click += new System.EventHandler(this.tst4_Click);
+            // 
+            // tst5
+            // 
+            this.tst5.Location = new System.Drawing.Point(88, 117);
+            this.tst5.Name = "tst5";
+            this.tst5.Size = new System.Drawing.Size(75, 23);
+            this.tst5.TabIndex = 11;
+            this.tst5.Text = "5tst";
+            this.tst5.UseVisualStyleBackColor = true;
+            this.tst5.Click += new System.EventHandler(this.tst5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 149);
+            this.Controls.Add(this.tst5);
+            this.Controls.Add(this.tst4);
+            this.Controls.Add(this.tst3);
+            this.Controls.Add(this.tst2);
+            this.Controls.Add(this.tst1);
+            this.Controls.Add(this.console_txt);
+            this.Controls.Add(this.connect_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button connect_btn;
+        private System.Windows.Forms.TextBox console_txt;
+        private System.Windows.Forms.Button tst1;
+        private System.Windows.Forms.Button tst2;
+        private System.Windows.Forms.Button tst3;
+        private System.Windows.Forms.Button tst4;
+        private System.Windows.Forms.Button tst5;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        
+        byte[] remdata = { };
+        TcpClient Client = new TcpClient();
+        string ip = "127.0.0.1"; // ip адрес
+        int port = 3000; // порт
+        Socket Sock;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_btn_Click(object sender, EventArgs e)
+        {
+            ip = "192.168.1.1";
+            try
+            {
+                Client.Connect(ip, port);
+            }
+            catch
+            {
+                console_txt.Text+="\nCannot connect to remote host!";
+                return;
+            }
+            console_txt.Text += "\nConnected'";
+            Sock = Client.Client;
+
+            Thread clientThread = new Thread(new ThreadStart(GetMsg));
+            clientThread.Start();
+
+        }
+
+
+        void GetMsg()
+        {
+            NetworkStream clientStream = Client.GetStream();
+            int bytesRead;
+            
+             while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(remdata, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+
+                // Convert the Bytes received to a string and display it on the Server Screen
+
+                // Now Echo the message back
+
+                //Echo(msg, encoder, clientStream);
+            }
+        }
+
+
+        private void send_btn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void stop_btn_Click(object sender, EventArgs e)
+        {
+            Sock.Close();
+            Client.Close();
+        }
+        ~Form1()
+        {
+            Sock.Close();
+            Client.Close();
+        }
+
+        private void tst1_Click(object sender, EventArgs e)
+        {
+            Sock.Send(Encoding.ASCII.GetBytes("1tst"));
+        }
+
+        private void tst2_Click(object sender, EventArgs e)
+        {
+            Sock.Send(Encoding.ASCII.GetBytes("2tst"));
+        }
+
+        private void tst3_Click(object sender, EventArgs e)
+        {
+            Sock.Send(Encoding.ASCII.GetBytes("3tst"));
+        }
+
+        private void tst4_Click(object sender, EventArgs e)
+        {
+            Sock.Send(Encoding.ASCII.GetBytes("4tst"));
+        }
+
+        private void tst5_Click(object sender, EventArgs e)
+        {
+            Sock.Send(Encoding.ASCII.GetBytes("5tst"));
+        }
+    }
+}
